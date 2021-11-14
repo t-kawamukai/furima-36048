955cbe29601b10c66252c0bce4f075c39331cfda
t-kawamukai <87261859+t-kawamukai@users.noreply.github.com>
2021-11-14 16:52:32 +0900

    商品情報削除機能の実装２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :item_set, only: [:show, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -24,7 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to new_user_session_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -37,8 +37,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path unless current_user.id == @item.user_id
     if @item.destroy
       redirect_to root_path
     else
@@ -53,5 +51,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def item_set
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
